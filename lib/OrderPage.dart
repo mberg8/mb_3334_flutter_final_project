0482ef5da6b006157612e5aa11cd4dec2ed06d4c
mberg8 <64221872+mberg8@users.noreply.github.com>
2021-06-07 02:32:49 -0500

    6/6. 2:31 AM. Updated OrderPage.dart. GridView. Scroll. TextAlign. fontSize. Added checkboxListTile for each book.

--- lib/OrderPage.dart
+++ lib/OrderPage.dart
@@ -1,40 +1,207 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-class OrderPage extends StatelessWidget {
+class OrderPage extends StatefulWidget {
+
+  @override
+  _PageState createState() => _PageState();
+
+}
+
+class _PageState  extends State<OrderPage> {
+  bool v1 = false;
+  bool v2 = false;
+  bool v3 = false;
+  bool v4 = false;
+  bool v5 = false;
+  bool v6 = false;
+  bool v7 = false;
+  bool v8 = false;
+  bool v9 = false;
+  bool v10 = false;
+  bool v11 = false;
+  bool v12 = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.tealAccent,
-      appBar: AppBar(
-        title: Text('Classic. Vintage. Books'),
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-
-      body: Center(
-        child: Container(
-
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        appBar: AppBar(
+          title: Text('Classic. Vintage. Books'),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: Center(
+          child: Container(
             child: GridView.count(
                 scrollDirection: Axis.vertical,
                 crossAxisCount: 1,
-                children: [
-            ElevatedButton(
-            child:
-            Text("Order Books"),
-            onPressed: (){
-              Navigator.push(context,
-                MaterialPageRoute(builder: (context) => OrderPage()),
-              );
-            }
+                padding: new EdgeInsets.all(1.0),
+                children: <Widget> [
+                  SizedBox(width: 10,),
+                  Text('Order Form', style: TextStyle(fontSize: 30.0), textAlign: TextAlign.center,),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                   title: const Text(
+                  'Anne of Green Gables', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                  value: this.v1,
+                  onChanged: (bool value) {
+                  setState(() {
+                    this.v1 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text(
+                    'Black Beauty', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v4,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v4 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text('Hatchet', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v4,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v4 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text('The Lion the Witch and the Wardrobe',
+                    style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v4,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v4 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text(
+                  'Little Women', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v5,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v5 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                     title: const Text(
+                    'Mary Poppins', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v6,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v6 = value;
+                  });
+                },
+              ),
+
+                  CheckboxListTile(
+                     secondary: const Icon(Icons.book),
+                    title: const Text(
+                  'The Wizard of Oz', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v7,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v7 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text(
+                  'Peter Pan', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v8,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v8 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text(
+                  'Pippi LongStalking', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v9,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v9 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text('Where the Red Fern Grows',
+                    style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v10,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v10 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text('Where the Red Fern Grows',
+                    style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                    value: this.v10,
+                    onChanged: (bool value) {
+                    setState(() {
+                    this.v10 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                      secondary: const Icon(Icons.book),
+                    title: const Text(
+                  'The Secret Garden', style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                  value: this.v11,
+                  onChanged: (bool value) {
+                  setState(() {
+                    this.v11 = value;
+                  });
+                },
+              ),
+                  CheckboxListTile(
+                    secondary: const Icon(Icons.book),
+                    title: const Text('Wild Hearts Cant be Broken',
+                  style: TextStyle(fontSize: 28.0), textAlign: TextAlign.center),
+                  value: this.v12,
+                  onChanged: (bool value) {
+                  setState(() {
+                    this.v12 = value;
+                  });
+                },
+              ),
+            ],
+
+
+          ),
+
+
         ),
-        ],
 
 
-            ),
-    ),
+      ),
     ),
     );
+  }
 }
-}
+
+
+
+
+
+
 
--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -6,144 +6,160 @@ import 'OrderPage.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: Colors.tealAccent,
-      appBar: AppBar(
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        appBar: AppBar(
         leading: IconButton(icon: Icon(Icons.menu), onPressed: (){
 
         }),
         title: Text('Classic. Vintage. Books'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search), onPressed:() {
+         IconButton(icon: Icon(Icons.search), onPressed:() {
 
 
-          }),
-        ],
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      body: Center(
-        child: Container(
-
-          // height: 200,
-          child: GridView.count(
-            scrollDirection: Axis.vertical,
-            crossAxisCount: 2,
-            children: [
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/Anne.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-
-
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/1Bb.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/Hatchet.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/Lion.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/littlew.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/maryp.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/oz.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/peter.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/pippi.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/redf.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/secretg.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-              ElevatedButton(
-                  child:
-                  Image.asset("assets/wildheart.png"),
-                  onPressed: (){
-                    Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => OrderPage()),
-                    );
-                  }
-              ),
-            ],
-          ),
+        }),
+          ],
+         backgroundColor: Colors.deepPurpleAccent,
         ),
+      body: Center(
+      child: Container(
+
+    // height: 200,
+      child: GridView.count(
+        scrollDirection: Axis.vertical,
+        crossAxisCount: 2,
+        children: [
+      ElevatedButton(
+        child:
+      Image.asset("assets/Anne.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+       MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+
+
       ),
+       ElevatedButton(
+        child:
+        Image.asset("assets/1Bb.png"),
+       style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+        onPressed: (){
+        Navigator.push(context,
+        MaterialPageRoute(builder: (context) => OrderPage()),
+        );
+      }
+        ),
+      ElevatedButton(
+       child:
+      Image.asset("assets/Hatchet.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+       ),
+       ElevatedButton(
+      child:
+      Image.asset("assets/Lion.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+      ),
+      ElevatedButton(
+       child:
+      Image.asset("assets/littlew.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+      ),
+      ElevatedButton(
+      child:
+      Image.asset("assets/maryp.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+      ),
+      ElevatedButton(
+      child:
+      Image.asset("assets/oz.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+      ),
+      ElevatedButton(
+      child:
+      Image.asset("assets/peter.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+      ),
+      ElevatedButton(
+      child:
+      Image.asset("assets/pippi.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+      ),
+      ElevatedButton(
+      child:
+      Image.asset("assets/redf.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+      ),
+      ElevatedButton(
+      child:
+      Image.asset("assets/secretg.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+      );
+      }
+      ),
+      ElevatedButton(
+      child:
+      Image.asset("assets/wildheart.png"),
+      style: ElevatedButton.styleFrom(primary: Colors.tealAccent),
+      onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => OrderPage()),
+    );
+    }
+    ),
+    ],
+    ),
+    ),
+    ),
+    ),
     );
 
+
   }
+
 }
